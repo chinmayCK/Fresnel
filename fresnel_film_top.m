313da98b60f5dd1546a06280388b30a70706cc04
Chinmay Khandekar <chinmayCK@users.noreply.github.com>
2021-01-20 13:44:13 -0500

    Update fresnel_film_top.m

--- fresnel_film_top.m
+++ fresnel_film_top.m
@@ -26,7 +26,7 @@ function[rss, rps, rsp, rpp, tss, tps, tsp, tpp]=fresnel_film_top(theta,phi,MM,d
   
   % pas should be 1   
   kp=sin(theta); 
-
+  phi=phi+pi;   % incidence along (theta, phi) means the parallel wavevector makes an angle phi+pi with x-axis
   % first find kz inside the material 
   kx=kp*cos(phi); ky=kp*sin(phi);
 
